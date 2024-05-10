df0f25312e7e70de7854f78159ac71ff3d0ad71d
Alexander Mann <alexandercmann@gmail.com>
2024-05-10 11:59:54 -0400

    ENG-0000 Fix: Portal Theme (Temporary) (#31)
    Temporary import theme preset via a relative path vs using the package

--- apps/portal/tailwind.config.js
+++ apps/portal/tailwind.config.js
@@ -1,10 +1,6 @@
-// import { themePreset } from '@intuition-ts/1ui'
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [],
-  // This breaks the build
-  // presets: [themePreset],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
--- apps/portal/vite.config.ts
+++ apps/portal/vite.config.ts
@@ -5,13 +5,16 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { flatRoutes } from 'remix-flat-routes'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+// TODO: Update this once we figure our the TS issue that vite is throwing
+// import { themePreset } from '@intuition-ts/1ui'
+import { themePreset } from '../../packages/1ui/src/styles/index'
 
 installGlobals()
 
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwindcss(), autoprefixer],
+      plugins: [tailwindcss(themePreset), autoprefixer],
     },
   },
   plugins: [
