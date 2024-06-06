4afa86fc01ed311c5b9158605fddb97215bffd73
Alexander Mann <alexandercmann@gmail.com>
2024-06-06 13:11:41 -0400

    ENG-1762 feat(1ui): create hover card component (#93)

---
+++ packages/1ui/src/components/HoverCard/HoverCard.spec.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { render } from '@testing-library/react'
+
+import { Button } from '..'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './HoverCard'
+
+describe('HoverCard', () => {
+  it('should render appropriate element', () => {
+    const { asFragment } = render(
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <Button>@super_dave</Button>
+        </HoverCardTrigger>
+        <HoverCardContent>content</HoverCardContent>
+      </HoverCard>,
+    )
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        <button
+          class="flex items-center gap-2 text-sm font-medium border-solid border disabled:bg-muted disabled:text-muted-foreground disabled:border-muted bg-primary text-primary-foreground border-primary hover:bg-primary/80 rounded-full shadow-md-subtle px-3 py-1"
+          data-state="closed"
+        >
+          @super_dave
+        </button>
+      </DocumentFragment>
+    `)
+  })
+})
---
+++ packages/1ui/src/components/HoverCard/HoverCard.stories.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar, AvatarFallback, AvatarImage, Button, Text } from '..'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './HoverCard'
+
+const meta: Meta<typeof HoverCard> = {
+  title: 'Components/HoverCard',
+  component: HoverCard,
+}
+
+export default meta
+
+type Story = StoryObj<typeof HoverCard>
+
+export const BasicUsage: Story = {
+  args: {},
+  render: (args) => (
+    <div className="h-[200px]">
+      <HoverCard {...args}>
+        <HoverCardTrigger asChild>
+          <Button variant="text">@super_dave</Button>
+        </HoverCardTrigger>
+        <HoverCardContent className="w-60">
+          <div className="flex gap-4">
+            <Avatar>
+              <AvatarImage src="https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg" />
+              <AvatarFallback>SD</AvatarFallback>
+            </Avatar>
+            <div>
+              <Text variant="body">Super Dave</Text>
+              <Text variant="caption" className="text-primary/50">
+                @super_dave
+              </Text>
+            </div>
+          </div>
+          <Text variant="caption">
+            I am Super Dave, the most super Dave you&apos;ll ever meet.
+          </Text>
+          <div className="flex gap-10">
+            <div>
+              <Text variant="body">9122</Text>
+              <Text variant="caption" className="text-primary/50">
+                followers
+              </Text>
+            </div>
+            <div>
+              <Text variant="body">1239</Text>
+              <Text variant="caption" className="text-primary/50">
+                following
+              </Text>
+            </div>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+    </div>
+  ),
+}
