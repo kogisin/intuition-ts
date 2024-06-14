d979ae36b589177b6b75d0a892c796d40c794bb5
Vital <rob@creative-element.net>
2024-06-14 02:05:23 -0400

    ENG-1792 feat(portal): get positions on identity detail (#112)
    ## Affected Packages
    
    Apps
    
    - [x] portal
    
    Packages
    
    - [ ] 1ui
    - [ ] api
    - [ ] protocol
    - [ ] sdk
    
    Tools
    
    - [ ] tools
    
    ## Overview
    
    - Updates the loader on identity/positions route to get positions on the
    identity.
    
    ## Screen Captures
    
    N/A
    
    ## Declaration
    
    - [x] I hereby declare that I have abided by the rules and regulations
    as outlined in the
    [CONTRIBUTING.md](https://github.com/0xIntuition/intuition-ts/blob/main/CONTRIBUTING.md)

--- apps/portal/app/routes/app+/identity+/$id+/positions.tsx
+++ apps/portal/app/routes/app+/identity+/$id+/positions.tsx
@@ -1,18 +1,84 @@
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import {
+  ApiError,
+  OpenAPI,
+  PositionSortColumn,
+  PositionsService,
+  SortDirection,
+} from '@0xintuition/api'
 
-export async function loader() {
+import { calculateTotalPages, getAuthHeaders } from '@lib/utils/misc'
+import { json, LoaderFunctionArgs } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { getPrivyAccessToken } from '@server/privy'
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  OpenAPI.BASE = 'https://dev.api.intuition.systems'
+  const accessToken = getPrivyAccessToken(request)
+  const headers = getAuthHeaders(accessToken !== null ? accessToken : '')
+  OpenAPI.HEADERS = headers as Record<string, string>
+  const id = params.id
+
+  if (!id) {
+    throw new Error('identity_id is undefined.')
+  }
+
+  const url = new URL(request.url)
+  const searchParams = new URLSearchParams(url.search)
+  const sortBy: PositionSortColumn =
+    (searchParams.get('sortBy') as PositionSortColumn) ?? 'CreatedAt'
+  const direction: SortDirection =
+    (searchParams.get('direction') as SortDirection) ?? 'desc'
+  const page = searchParams.get('page')
+    ? parseInt(searchParams.get('page') as string)
+    : 1
+  const limit = searchParams.get('limit') ?? '10'
+
+  let positions
+  try {
+    positions = await PositionsService.searchPositions({
+      identity: id,
+      paging: {
+        page: page,
+        limit: Number(limit),
+        offset: 0,
+      },
+      sort: {
+        sortBy: sortBy,
+        direction: direction,
+      },
+    })
+  } catch (error: unknown) {
+    if (error instanceof ApiError) {
+      positions = undefined
+      console.log(`${error.name} - ${error.status}: ${error.message}`)
+    } else {
+      throw error
+    }
+  }
+
+  const totalPages = calculateTotalPages(positions?.total ?? 0, Number(limit))
+
+  console.log('positions', positions)
+
   return json({
-    message: 'hack the planet',
+    positions,
+    sortBy,
+    direction,
+    pagination: {
+      page: Number(page),
+      limit: Number(limit),
+      total: positions?.total,
+      totalPages,
+    },
   })
 }
 
 export default function IdentityPositions() {
-  const { message } = useLoaderData<typeof loader>()
+  const { positions } = useLoaderData<typeof loader>()
   return (
     <div className="m-8 flex flex-col items-center gap-4">
       <div className="flex flex-col">Identity Positions Route</div>
-      <div>{message}</div>
+      <div>{JSON.stringify(positions)}</div>
     </div>
   )
 }
