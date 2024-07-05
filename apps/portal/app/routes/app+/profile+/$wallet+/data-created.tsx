a1651be772aa8b9106069a03b5460503f7a658cf
Vital <rob@creative-element.net>
2024-07-05 11:34:16 -0400

    ENG-2174 feat(portal): shows identity positions created by user on profile subroute data created (#199)
    ## Affected Packages
    
    Apps
    
    - [x] portal
    
    Packages
    
    - [ ] 1ui
    - [ ] api
    - [ ] protocol
    - [ ] sdk
    
    Tools
    
    - [ ] tools
    
    ## Overview
    
    - Added positions list for positions created by user identity on profile
    subroute data created.
    
    Note; Similar to the other instances of lists like this, sorting and
    searching still has to be worked out.
    
    ## Screen Captures
    
    If applicable, add screenshots or screen captures of your changes.
    
    ## Declaration
    
    - [x] I hereby declare that I have abided by the rules and regulations
    as outlined in the
    [CONTRIBUTING.md](https://github.com/0xIntuition/intuition-ts/blob/main/CONTRIBUTING.md)

--- apps/portal/app/routes/app+/profile+/$wallet+/data-created.tsx
+++ apps/portal/app/routes/app+/profile+/$wallet+/data-created.tsx
@@ -1,19 +1,409 @@
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { useEffect, useState } from 'react'
 
-export async function loader() {
+import {
+  IdentityPosition,
+  IdentityTag,
+  Input,
+  Pagination,
+  PaginationContent,
+  PaginationFirst,
+  PaginationItem,
+  PaginationLast,
+  PaginationNext,
+  PaginationPageCounter,
+  PaginationPrevious,
+  PaginationRowSelection,
+  PaginationSummary,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@0xintuition/1ui'
+import {
+  ApiError,
+  ClaimSortColumn,
+  ClaimsService,
+  IdentitiesService,
+  OpenAPI,
+  PositionPresenter,
+  PositionSortColumn,
+  PositionsService,
+  SortDirection,
+  UsersService,
+} from '@0xintuition/api'
+
+import { useLiveLoader } from '@lib/hooks/useLiveLoader'
+import logger from '@lib/utils/logger'
+import {
+  calculateTotalPages,
+  formatBalance,
+  getAuthHeaders,
+} from '@lib/utils/misc'
+import { json, LoaderFunctionArgs } from '@remix-run/node'
+import { useFetcher, useSearchParams } from '@remix-run/react'
+import { getPrivyAccessToken } from '@server/privy'
+import { formatUnits } from 'viem'
+
+export async function loader({ params, request }: LoaderFunctionArgs) {
+  OpenAPI.BASE = 'https://dev.api.intuition.systems'
+  const accessToken = getPrivyAccessToken(request)
+  const headers = getAuthHeaders(accessToken !== null ? accessToken : '')
+  OpenAPI.HEADERS = headers as Record<string, string>
+
+  const wallet = params.wallet
+
+  if (!wallet) {
+    throw new Error('Wallet is undefined.')
+  }
+
+  let userIdentity
+  try {
+    userIdentity = await IdentitiesService.getIdentityById({
+      id: wallet,
+    })
+  } catch (error: unknown) {
+    if (error instanceof ApiError) {
+      userIdentity = undefined
+      logger(`${error.name} - ${error.status}: ${error.message} ${error.url}`)
+    } else {
+      throw error
+    }
+  }
+
+  let userObject
+  try {
+    userObject = await UsersService.getUserByWallet({
+      wallet: wallet,
+    })
+  } catch (error: unknown) {
+    if (error instanceof ApiError) {
+      userObject = undefined
+      logger(`${error.name} - ${error.status}: ${error.message} ${error.url}`)
+    } else {
+      throw error
+    }
+  }
+
+  if (!userObject) {
+    return logger('No user found in DB')
+  }
+
+  let userTotals
+  try {
+    userTotals = await UsersService.getUserTotals({
+      id: userObject.id,
+    })
+  } catch (error: unknown) {
+    if (error instanceof ApiError) {
+      userTotals = undefined
+      logger(`${error.name} - ${error.status}: ${error.message} ${error.url}`)
+    } else {
+      throw error
+    }
+  }
+
+  const url = new URL(request.url)
+  const searchParams = new URLSearchParams(url.search)
+  // const search = searchParams.get('search')
+  const sortBy = searchParams.get('sortBy') ?? 'UpdatedAt'
+  const direction = searchParams.get('direction') ?? 'asc'
+  const page = searchParams.get('page')
+    ? parseInt(searchParams.get('page') as string)
+    : 1
+  const limit = searchParams.get('limit') ?? '10'
+
+  let positions
+  try {
+    positions = await PositionsService.searchPositions({
+      creator: wallet,
+      paging: {
+        page: page,
+        limit: Number(limit),
+        offset: 0,
+      },
+      sort: {
+        sortBy: sortBy as PositionSortColumn,
+        direction: direction as SortDirection,
+      },
+    })
+  } catch (error: unknown) {
+    if (error instanceof ApiError) {
+      positions = undefined
+      console.log(`${error.name} - ${error.status}: ${error.message}`)
+    } else {
+      throw error
+    }
+  }
+
+  let claims
+  try {
+    claims = await ClaimsService.searchClaims({
+      identity: userIdentity?.id,
+      page: page,
+      limit: Number(limit),
+      offset: 0,
+      sortBy: sortBy as ClaimSortColumn,
+      direction: direction as SortDirection,
+    })
+  } catch (error: unknown) {
+    if (error instanceof ApiError) {
+      claims = undefined
+      console.log(`${error.name} - ${error.status}: ${error.message}`)
+    } else {
+      throw error
+    }
+  }
+
+  const totalPages = calculateTotalPages(positions?.total ?? 0, Number(limit))
+
+  console.log('positions', positions)
+
+  // console.log('search', search)
+  // console.log('sortBy', sortBy)
+  // console.log('direction', direction)
+  // console.log('page', page)
+  // console.log('limit', limit)
+
   return json({
-    message: 'hack the planet',
+    userIdentity,
+    userObject,
+    userTotals,
+    positions,
+    claims,
+    sortBy,
+    direction,
+    pagination: {
+      page: Number(page),
+      limit: Number(limit),
+      total: positions?.total,
+      totalPages,
+    },
   })
 }
 
 export default function ProfileDataCreated() {
-  const { message } = useLoaderData<typeof loader>()
+  return (
+    <div className="flex-col justify-start items-start flex w-full">
+      <div className="self-stretch justify-between items-center inline-flex mb-4">
+        <div className="grow shrink basis-0 text-white text-xl font-medium leading-[30px]">
+          Active Positions
+        </div>
+      </div>
+      <PositionsByIdentity />
+    </div>
+  )
+}
+
+export function PositionsByIdentity() {
+  const initialData = useLiveLoader<typeof loader>(['attest'])
+  const { userIdentity, userTotals, pagination } = initialData
+  const fetcher = useFetcher<typeof loader>()
+  const [positions, setPositions] = useState<PositionPresenter[]>(
+    initialData.positions?.data ?? [],
+  )
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  console.log('fetcher.data', fetcher.data)
+  useEffect(() => {
+    if (fetcher.data) {
+      setPositions(fetcher.data.positions?.data as PositionPresenter[])
+    }
+  }, [fetcher.data])
+
+  useEffect(() => {
+    setPositions(initialData.positions?.data as PositionPresenter[])
+  }, [initialData.positions])
+
+  const options = [
+    { value: 'Updated At', sortBy: 'UpdatedAt' },
+    { value: 'Total ETH', sortBy: 'Assets' },
+  ]
+
+  const handleSortChange = (
+    newSortBy: PositionSortColumn,
+    newDirection: SortDirection,
+  ) => {
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      sortBy: newSortBy,
+      direction: newDirection,
+      page: '1',
+    })
+  }
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchValue = event.target.value
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      search: newSearchValue,
+      page: '1',
+    })
+  }
+
+  const onPageChange = (newPage: number) => {
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      page: newPage.toString(),
+    })
+  }
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      <pre>Profile Data-Created Route</pre>
-      data created loader: {message}
-    </div>
+    <>
+      <div className="h-[184px] flex-col justify-start items-start gap-3 flex w-full">
+        <div className="self-stretch justify-start items-start gap-4 inline-flex">
+          <div className="grow shrink basis-0 self-stretch p-6 bg-black rounded-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex">
+            <div className="self-stretch justify-start items-start gap-5 inline-flex">
+              <div className="justify-start items-center gap-1.5 flex">
+                <div className="text-white/60 text-sm font-normal leading-tight">
+                  Identities staked on by
+                </div>
+                <IdentityTag
+                  imgSrc={userIdentity?.user?.image ?? userIdentity?.image}
+                  variant={userIdentity?.user ? 'user' : 'non-user'}
+                >
+                  <span className="min-w-20 text-ellipsis">
+                    {userIdentity?.user?.display_name ??
+                      userIdentity?.display_name}
+                  </span>
+                </IdentityTag>
+              </div>
+            </div>
+            <div className="self-stretch justify-between items-start inline-flex">
+              <div className="flex-col justify-start items-end inline-flex">
+                <div className="self-stretch text-white/60 text-xs font-normal leading-[18px]">
+                  Identities
+                </div>
+                <div className="self-stretch text-white text-xl font-medium leading-[30px]">
+                  {userTotals?.total_positions ?? '0'}
+                </div>
+              </div>
+              <div className="flex-col justify-start items-end inline-flex">
+                <div className="self-stretch text-white/60 text-xs font-normal leading-[18px]">
+                  Total Staked
+                </div>
+                <div className="self-stretch text-white text-xl font-medium leading-[30px]">
+                  {formatBalance(
+                    userTotals?.total_position_value ?? '0',
+                    18,
+                    4,
+                  )}{' '}
+                  ETH
+                </div>
+              </div>
+              <div className="flex-col justify-start items-end inline-flex">
+                <div className="self-stretch text-right text-white/60 text-xs font-normal leading-[18px]">
+                  Fees Accrued
+                </div>
+                <div className="self-stretch text-right text-success text-xl font-medium leading-[30px]">
+                  +
+                  {(
+                    +formatBalance(userTotals?.total_delta ?? '0', 18, 4) -
+                    +formatBalance(
+                      userTotals?.total_position_value ?? '0',
+                      18,
+                      4,
+                    )
+                  ).toFixed(4)}{' '}
+                  ETH
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between w-full mt-6">
+        <Input className="w-[196px]" onChange={handleSearchChange} />
+        <Select
+          onValueChange={(value) => {
+            const selectedOption = options.find(
+              (option) => option.value.toLowerCase() === value,
+            )
+            if (selectedOption) {
+              handleSortChange(selectedOption.sortBy, 'desc')
+            }
+          }}
+        >
+          <SelectTrigger className="w-[200px] rounded-xl border border-primary-600 bg-primary-50/5 text-card-foreground transition-colors duration-150 hover:cursor-pointer hover:border-primary-400 hover:bg-primary-50/10 hover:text-primary-foreground">
+            <SelectValue placeholder={`Sort by`} />
+          </SelectTrigger>
+          <SelectContent>
+            {options.map((option) => (
+              <SelectItem
+                key={option.value.toLowerCase()}
+                value={option.value.toLowerCase()}
+              >
+                {option.value}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="mt-6 flex flex-col w-full">
+        {positions?.map((position) => (
+          <div
+            key={position.id}
+            className={`grow shrink basis-0 self-stretch p-6 bg-black first:rounded-t-xl last:rounded-b-xl border border-neutral-300/20 flex-col justify-start items-start gap-5 inline-flex`}
+          >
+            <IdentityPosition
+              variant="user"
+              avatarSrc={position.user?.image}
+              name={position.user?.display_name}
+              walletAddress={position.user?.wallet}
+              amount={formatBalance(BigInt(position.assets), 18, 4)}
+              feesAccrued={Number(
+                formatUnits(BigInt(+position.assets - +position.value), 18),
+              )}
+              updatedAt={position.updated_at}
+            />
+          </div>
+        ))}
+      </div>
+      <Pagination className="flex w-full justify-between my-4">
+        <PaginationSummary
+          totalEntries={pagination.total ?? 0}
+          label="positions"
+        />
+        <div className="flex">
+          <PaginationRowSelection defaultValue="10" />
+          <PaginationPageCounter
+            currentPage={pagination.page ?? 0}
+            totalPages={pagination.totalPages ?? 0}
+          />
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationFirst
+                href="#"
+                onClick={() => onPageChange(1)}
+                disabled={pagination.page === 1}
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationPrevious
+                href="#"
+                onClick={() => onPageChange(pagination.page - 1)}
+                disabled={
+                  pagination.page === 1 || pagination.page === undefined
+                }
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext
+                href="#"
+                onClick={() => onPageChange(pagination.page + 1)}
+                disabled={pagination.page === pagination.totalPages}
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLast
+                href="#"
+                onClick={() => onPageChange(pagination.totalPages)}
+                disabled={pagination.page === pagination.totalPages}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </div>
+      </Pagination>
+    </>
   )
 }
