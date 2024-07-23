37cb103609e4e45f2218eee3b3e25c8e952a41e9
Alexander Mann <alexandercmann@gmail.com>
2024-07-23 15:16:32 -0400

    ENG-0000 fix(portal): comment out privy-logout (#338)
    ## Affected Packages
    
    Apps
    
    - [x] portal
    
    Packages
    
    - [ ] 1ui
    - [ ] api
    - [ ] protocol
    - [ ] sdk

--- apps/portal/app/.client/privy-logout.tsx
+++ apps/portal/app/.client/privy-logout.tsx
@@ -1,27 +1,29 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
 
-import { usePrivy } from '@privy-io/react-auth'
-import { useSubmit } from '@remix-run/react'
-import { useAccount, useDisconnect } from 'wagmi'
+// import { usePrivy } from '@privy-io/react-auth'
+// import { useSubmit } from '@remix-run/react'
+// import { useAccount, useDisconnect } from 'wagmi'
 
-export default function PrivyLogout({ wallet }: { wallet: string }) {
-  const { address } = useAccount()
-  const submit = useSubmit()
-  const { logout } = usePrivy()
-  const { disconnect } = useDisconnect()
-  useEffect(() => {
-    const handleLogout = async () => {
-      if (address !== wallet) {
-        await logout()
-        disconnect()
-        submit(null, {
-          action: '/actions/logout',
-          method: 'post',
-        })
-      }
-    }
-    handleLogout()
-  }, [address, wallet, submit, logout, disconnect])
+// TODO: Fix constant logout issue [ENG-2776]
+export default function PrivyLogout() {
+  // export default function PrivyLogout({ wallet }: { wallet: string }) {
+  // const { address } = useAccount()
+  // const submit = useSubmit()
+  // const { logout } = usePrivy()
+  // const { disconnect } = useDisconnect()
+  // useEffect(() => {
+  //   const handleLogout = async () => {
+  //     if (address !== wallet) {
+  //       await logout()
+  //       disconnect()
+  //       submit(null, {
+  //         action: '/actions/logout',
+  //         method: 'post',
+  //       })
+  //     }
+  //   }
+  //   handleLogout()
+  // }, [address, wallet, submit, logout, disconnect])
 
   return <div></div>
 }
