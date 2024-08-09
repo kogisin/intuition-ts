a256c58d1c63bf64202c5e11a0ceeb1a8b2c6df8
Jonathan Prozzi <jonathanprozzi@gmail.com>
2024-08-09 01:44:14 -0400

    ENG-3161 feat(portal): add make claim button to data about pages (#550)
    - Adds the Make a Claim (and modal) to the data about routes on profile
    and profile/$wallet

--- apps/portal/app/routes/app+/profile+/_index+/data-about.tsx
+++ apps/portal/app/routes/app+/profile+/_index+/data-about.tsx
@@ -1,8 +1,9 @@
 import { Suspense } from 'react'
 
-import { ErrorStateCard, Text } from '@0xintuition/1ui'
+import { Button, ErrorStateCard, Text } from '@0xintuition/1ui'
 import { ClaimsService } from '@0xintuition/api'
 
+import CreateClaimModal from '@components/create-claim/create-claim-modal'
 import { ClaimsList as ClaimsAboutIdentity } from '@components/list/claims'
 import { PositionsOnIdentity } from '@components/list/positions-on-identity'
 import DataAboutHeader from '@components/profile/data-about-header'
@@ -11,13 +12,14 @@ import { DataHeaderSkeleton, PaginatedListSkeleton } from '@components/skeleton'
 import { useLiveLoader } from '@lib/hooks/useLiveLoader'
 import { getClaimsAboutIdentity } from '@lib/services/claims'
 import { getPositionsOnIdentity } from '@lib/services/positions'
-import logger from '@lib/utils/logger'
+import { createClaimModalAtom } from '@lib/state/store'
 import { formatBalance, invariant } from '@lib/utils/misc'
 import { defer, LoaderFunctionArgs } from '@remix-run/node'
 import { Await, useRouteLoaderData } from '@remix-run/react'
 import { fetchWrapper } from '@server/api'
 import { requireUserWallet } from '@server/auth'
 import { NO_USER_IDENTITY_ERROR, NO_WALLET_ERROR } from 'app/consts'
+import { useAtom } from 'jotai'
 
 import { ProfileLoaderData } from './_layout'
 
@@ -45,13 +47,14 @@ export async function loader({ request }: LoaderFunctionArgs) {
         identity: userWallet,
       },
     }),
+    userWallet,
   })
 }
 
 export default function ProfileDataAbout() {
-  const { positions, claims, claimsSummary } = useLiveLoader<typeof loader>([
-    'attest',
-  ])
+  const { positions, claims, claimsSummary, userWallet } = useLiveLoader<
+    typeof loader
+  >(['attest'])
 
   const { userIdentity } =
     useRouteLoaderData<ProfileLoaderData>(
@@ -59,103 +62,122 @@ export default function ProfileDataAbout() {
     ) ?? {}
   invariant(userIdentity, NO_USER_IDENTITY_ERROR)
 
-  logger('data-about render')
+  const [createClaimModalActive, setCreateClaimModalActive] =
+    useAtom(createClaimModalAtom)
 
   return (
-    <div className="flex-col justify-start items-start flex w-full gap-6">
-      <div className="flex flex-col w-full gap-6">
-        <div className="self-stretch justify-between items-center inline-flex">
-          <Text
-            variant="headline"
-            weight="medium"
-            className="theme-secondary-foreground w-full"
-          >
-            Claims about this Identity
-          </Text>
+    <>
+      <div className="flex-col justify-start items-start flex w-full gap-6">
+        <div className="flex flex-col w-full gap-6">
+          <div className="flex max-lg:flex-col justify-between items-center max-lg:w-full">
+            <div className="self-stretch justify-between items-center inline-flex">
+              <Text
+                variant="headline"
+                weight="medium"
+                className="theme-secondary-foreground w-full"
+              >
+                Claims about this Identity
+              </Text>
+            </div>
+            <Button
+              variant="primary"
+              className="max-lg:w-full max-lg:mt-2"
+              onClick={() => setCreateClaimModalActive(true)}
+            >
+              Make a Claim
+            </Button>
+          </div>
+          <Suspense fallback={<DataHeaderSkeleton />}>
+            <Await resolve={claims} errorElement={<></>}>
+              {(resolvedClaims) => (
+                <Await resolve={claimsSummary} errorElement={<></>}>
+                  {(resolvedClaimsSummary) => (
+                    <DataAboutHeader
+                      variant="claims"
+                      userIdentity={userIdentity}
+                      totalClaims={resolvedClaims.pagination.totalEntries}
+                      totalStake={
+                        +formatBalance(
+                          resolvedClaimsSummary?.assets_sum ?? 0,
+                          18,
+                          4,
+                        )
+                      }
+                    />
+                  )}
+                </Await>
+              )}
+            </Await>
+          </Suspense>
+          <Suspense fallback={<PaginatedListSkeleton />}>
+            <Await
+              resolve={claims}
+              errorElement={
+                <ErrorStateCard>
+                  <RevalidateButton />
+                </ErrorStateCard>
+              }
+            >
+              {(resolvedClaims) => (
+                <ClaimsAboutIdentity
+                  claims={resolvedClaims.data}
+                  pagination={resolvedClaims.pagination}
+                  paramPrefix="claims"
+                  enableSearch
+                  enableSort
+                />
+              )}
+            </Await>
+          </Suspense>
         </div>
-        <Suspense fallback={<DataHeaderSkeleton />}>
-          <Await resolve={claims} errorElement={<></>}>
-            {(resolvedClaims) => (
-              <Await resolve={claimsSummary} errorElement={<></>}>
-                {(resolvedClaimsSummary) => (
-                  <DataAboutHeader
-                    variant="claims"
-                    userIdentity={userIdentity}
-                    totalClaims={resolvedClaims.pagination.totalEntries}
-                    totalStake={
-                      +formatBalance(
-                        resolvedClaimsSummary?.assets_sum ?? 0,
-                        18,
-                        4,
-                      )
-                    }
-                  />
-                )}
-              </Await>
-            )}
-          </Await>
-        </Suspense>
-        <Suspense fallback={<PaginatedListSkeleton />}>
-          <Await
-            resolve={claims}
-            errorElement={
-              <ErrorStateCard>
-                <RevalidateButton />
-              </ErrorStateCard>
-            }
-          >
-            {(resolvedClaims) => (
-              <ClaimsAboutIdentity
-                claims={resolvedClaims.data}
-                pagination={resolvedClaims.pagination}
-                paramPrefix="claims"
-                enableSearch
-                enableSort
-              />
-            )}
-          </Await>
-        </Suspense>
-      </div>
-      <div className="flex flex-col pt-4 w-full gap-6">
-        <div className="self-stretch justify-between items-center inline-flex">
-          <Text
-            variant="headline"
-            weight="medium"
-            className="theme-secondary-foreground w-full"
-          >
-            Positions on this Identity
-          </Text>
+        <div className="flex flex-col pt-4 w-full gap-6">
+          <div className="self-stretch justify-between items-center inline-flex">
+            <Text
+              variant="headline"
+              weight="medium"
+              className="theme-secondary-foreground w-full"
+            >
+              Positions on this Identity
+            </Text>
+          </div>
+          <Suspense fallback={<DataHeaderSkeleton />}>
+            <Await resolve={positions} errorElement={<></>}>
+              {(resolvedPositions) => (
+                <DataAboutHeader
+                  variant="positions"
+                  userIdentity={userIdentity}
+                  totalPositions={resolvedPositions.pagination.totalEntries}
+                  totalStake={+formatBalance(userIdentity.assets_sum, 18, 4)}
+                />
+              )}
+            </Await>
+          </Suspense>
+          <Suspense fallback={<PaginatedListSkeleton />}>
+            <Await
+              resolve={positions}
+              errorElement={
+                <ErrorStateCard>
+                  <RevalidateButton />
+                </ErrorStateCard>
+              }
+            >
+              {(resolvedPositions) => (
+                <PositionsOnIdentity
+                  positions={resolvedPositions.data}
+                  pagination={resolvedPositions.pagination}
+                />
+              )}
+            </Await>
+          </Suspense>
         </div>
-        <Suspense fallback={<DataHeaderSkeleton />}>
-          <Await resolve={positions} errorElement={<></>}>
-            {(resolvedPositions) => (
-              <DataAboutHeader
-                variant="positions"
-                userIdentity={userIdentity}
-                totalPositions={resolvedPositions.pagination.totalEntries}
-                totalStake={+formatBalance(userIdentity.assets_sum, 18, 4)}
-              />
-            )}
-          </Await>
-        </Suspense>
-        <Suspense fallback={<PaginatedListSkeleton />}>
-          <Await
-            resolve={positions}
-            errorElement={
-              <ErrorStateCard>
-                <RevalidateButton />
-              </ErrorStateCard>
-            }
-          >
-            {(resolvedPositions) => (
-              <PositionsOnIdentity
-                positions={resolvedPositions.data}
-                pagination={resolvedPositions.pagination}
-              />
-            )}
-          </Await>
-        </Suspense>
       </div>
-    </div>
+      {userWallet && (
+        <CreateClaimModal
+          open={createClaimModalActive}
+          wallet={userWallet}
+          onClose={() => setCreateClaimModalActive(false)}
+        />
+      )}
+    </>
   )
 }
