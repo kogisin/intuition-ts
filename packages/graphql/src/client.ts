82bd23e5b37003baf01d8ad3891c23af64c2fa8c
Jonathan Prozzi <jonathanprozzi@gmail.com>
2025-01-08 22:19:41 -0500

    ENG-5376 feat(graphql): update constants.ts with new testnet deployment url (#1022)
    - Updates the exported `API_URL_DEV` in `constants.ts` to use the new
    deployment URL
    - Switches the default URL for the client as `API_URL_PROD` -- this is
    what it's been since Dev and Prod were the same URL
    - Consumers can leverage the `configureClient` approach to override /
    set to `API_URL_DEV` in the consuming application:
    https://tech.docs.intuition.systems/dev/graphql-onboarding (the process
    is the same, but this page will be updated to note that the Dev/Prod
    URLs are now different)

--- packages/graphql/src/client.ts
+++ packages/graphql/src/client.ts
@@ -1,13 +1,13 @@
 import { GraphQLClient } from 'graphql-request'
 
-import { API_URL_DEV } from './constants'
+import { API_URL_PROD } from './constants'
 
 export interface ClientConfig {
   headers: HeadersInit
   apiUrl?: string
 }
 
-const DEFAULT_API_URL = API_URL_DEV
+const DEFAULT_API_URL = API_URL_PROD
 
 let globalConfig: { apiUrl?: string } = {
   apiUrl: DEFAULT_API_URL,
